28d64f06dc20d4a19e74b5f4961a1647f2f0b2b2
Gaurav Khanwani <40686985+khanwanigaurav@users.noreply.github.com>
2018-06-29 14:21:15 +0530

    Add files via upload

---
+++ script/demo.js
@@ -0,0 +1,69 @@
+var shops = document.getElementById("shops");
+var markets = document.getElementById("markets");
+var malls = document.getElementById("malls");
+var sales = document.getElementById("sales");
+var login = document.getElementById("login");
+var logo = document.getElementById("logo");
+
+// Get the modal
+var modal = document.getElementById('id01');
+//model got
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+/*var btn_search_text = document.getElementById("btn_search_text");
+function btn_search_text_in()  {
+	shops.style.display = "none";
+	markets.style.display = "none";
+	malls.style.display = "none";
+	sales.style.display = "none";
+	login.style.display = "none";
+	logo.style.display = "none";
+	btn_search.style.display = "none";
+
+}
+
+function btn_search_text_out()  {
+	shops.style.display = "inline";
+	markets.style.display = "inline";
+	malls.style.display = "inline";
+	sales.style.display = "inline";
+	login.style.display = "inline";
+	logo.style.display = "inline";
+	btn_search.style.display = "inline"
+}
+
+
+var btn_search1 = document.getElementById("btn_search1");*/
+var logo1 = document.getElementById("logo1");
+var logo2 = document.getElementById("logo2");
+
+/*function btn_search_text0_in()  {
+	logo1.style.opacity = 0;
+	logo2.style.opacity = 0;
+    btn_search_text1.style.display = "block";
+
+}
+
+function btn_search_text0_out()  {
+	logo1.style.opacity = 1;
+	logo2.style.opacity = 1;
+	btn_search_text1.style.display = "none";
+
+}
+*/
+/*function btn_text_new_in() {
+	btn_search_text.style.display = "inline";
+}
+
+
+function btn_text_new_out() {
+	btn_search_text.style.display = "none";
+}*/
+
+
+
---
+++ script/new_page.js
@@ -0,0 +1,127 @@
+   var panel_main_1_1 = document.getElementById("panel_main_1_1");
+   var panel_main_1_2 = document.getElementById("panel_main_1_2");
+   var panel_main_1_3 = document.getElementById("panel_main_1_3");
+   var panel_main_2_1 = document.getElementById("panel_main_2_1");
+   var panel_main_2_2 = document.getElementById("panel_main_2_2");
+   var panel_main_2_3 = document.getElementById("panel_main_2_3");
+
+   function panel_main_1_1_in() {
+    panel_main_1_2.style.opacity = 0.3;
+    panel_main_1_3.style.opacity = 0.3;
+    panel_main_2_1.style.opacity = 0.3;
+    panel_main_2_2.style.opacity = 0.3;
+    panel_main_2_3.style.opacity = 0.3;
+   }
+
+   function panel_main_1_1_out() {
+    panel_main_1_2.style.opacity = 1;
+    panel_main_1_3.style.opacity = 1;
+    panel_main_2_1.style.opacity = 1;
+    panel_main_2_2.style.opacity = 1;
+    panel_main_2_3.style.opacity = 1;
+   }
+
+
+
+
+   function panel_main_1_2_in() {
+    panel_main_1_1.style.opacity = 0.3;
+    panel_main_1_3.style.opacity = 0.3;
+    panel_main_2_1.style.opacity = 0.3;
+    panel_main_2_2.style.opacity = 0.3;
+    panel_main_2_3.style.opacity = 0.3;
+   }
+
+   function panel_main_1_2_out() {
+    panel_main_1_1.style.opacity = 1;
+    panel_main_1_3.style.opacity = 1;
+    panel_main_2_1.style.opacity = 1;
+    panel_main_2_2.style.opacity = 1;
+    panel_main_2_3.style.opacity = 1;
+   }
+
+
+
+
+   function panel_main_1_3_in() {
+    panel_main_1_1.style.opacity = 0.3;
+    panel_main_1_2.style.opacity = 0.3;
+    panel_main_2_1.style.opacity = 0.3;
+    panel_main_2_2.style.opacity = 0.3;
+    panel_main_2_3.style.opacity = 0.3;
+   }
+
+   function panel_main_1_3_out() {
+    panel_main_1_1.style.opacity = 1;
+    panel_main_1_2.style.opacity = 1;
+    panel_main_2_1.style.opacity = 1;
+    panel_main_2_2.style.opacity = 1;
+    panel_main_2_3.style.opacity = 1;
+   }
+
+
+
+
+   function panel_main_2_1_in() {
+    panel_main_1_1.style.opacity = 0.3;
+    panel_main_1_2.style.opacity = 0.3;
+    panel_main_1_3.style.opacity = 0.3;
+    panel_main_2_2.style.opacity = 0.3;
+    panel_main_2_3.style.opacity = 0.3;
+   }
+
+   function panel_main_2_1_out() {
+    panel_main_1_1.style.opacity = 1;
+    panel_main_1_2.style.opacity = 1;
+    panel_main_1_3.style.opacity = 1;
+    panel_main_2_2.style.opacity = 1;
+    panel_main_2_3.style.opacity = 1;
+   }
+
+
+
+
+   function panel_main_2_2_in() {
+    panel_main_1_1.style.opacity = 0.3;
+    panel_main_1_2.style.opacity = 0.3;
+    panel_main_1_3.style.opacity = 0.3;
+    panel_main_2_1.style.opacity = 0.3;
+    panel_main_2_3.style.opacity = 0.3;
+   }
+
+   function panel_main_2_2_out() {
+    panel_main_1_1.style.opacity = 1;
+    panel_main_1_2.style.opacity = 1;
+    panel_main_1_3.style.opacity = 1;
+    panel_main_2_1.style.opacity = 1;
+    panel_main_2_3.style.opacity = 1;
+   }
+
+
+
+
+   function panel_main_2_3_in() {
+    panel_main_1_1.style.opacity = 0.3;
+    panel_main_1_2.style.opacity = 0.3;
+    panel_main_1_3.style.opacity = 0.3;
+    panel_main_2_1.style.opacity = 0.3;
+    panel_main_2_2.style.opacity = 0.3;
+   }
+
+   function panel_main_2_3_out() {
+    panel_main_1_1.style.opacity = 1;
+    panel_main_1_2.style.opacity = 1;
+    panel_main_1_3.style.opacity = 1;
+    panel_main_2_1.style.opacity = 1;
+    panel_main_2_2.style.opacity = 1;
+   }
+
+
+   //end to this
+
+
+
+   var navigate = document.getElementById("navigate");
+   function navigate_in() {
+    navigate.style.border = "2px #2196f3";
+   }
